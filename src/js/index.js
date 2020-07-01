a467db167733af63292bfbb470556ffa9fc25d29
pablokmieleski <pablo.kmieleski@gmail.com>
2020-07-01 19:39:15 +0100

    new concept

--- src/js/index.js
+++ src/js/index.js
@@ -8,20 +8,34 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀')
 
-const NAV = document.querySelector('nav');
-const ITEM = document.querySelectorAll('nav ul li');
-const HBUTTON = document.querySelector('#hamburger');
+// const NAV = document.querySelector('nav');
+//const ITEM = document.querySelectorAll('nav ul li');
+//const HBUTTON = document.querySelector('#hamburger');
 
-HBUTTON.onclick = function() {
-    this.classList.toggle('trans');
-    NAV.classList.toggle('show');
-};
+//HBUTTON.onclick = function() {
+//    this.classList.toggle('trans');
+//   NAV.classList.toggle('show');
+//};
 
-function actionMenu() {
-    NAV.classList.remove('show');
-    HBUTTON.classList.remove('trans');
-}
+//function actionMenu() {
+//    NAV.classList.remove('show');
+//   HBUTTON.classList.remove('trans');
+//}
 
-ITEM[0].onclick = actionMenu;
-ITEM[1].onclick = actionMenu;
-ITEM[2].onclick = actionMenu;
+//ITEM[0].onclick = actionMenu;
+//ITEM[1].onclick = actionMenu;
+//ITEM[2].onclick = actionMenu;
+
+
+const hamburger = document.querySelector('.hamburger--js');
+const navigationX = document.querySelector('.navigation__list--js');
+
+hamburger.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+    if (navigationX.classList.contains('navigation__list--visible')) {
+        hamburger.innerHTML = 'X';
+    } else {
+        hamburger.innerHTML = '≡';
+    }
+});
